1eeb7c146713f9f235e6e5ba6e6db23cd8742ba5
김나연 <nayeonkinn@gmail.com>
2022-08-03 21:39:12 +0900

    220803

---
+++ baekjoon/[실버 Ⅲ] 2559. 수열.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+N, K = list(map(int, input().split()))
+ondo = [num for num in list(map(int, input().split()))]
+total = [sum(ondo[:K])]
+for i in range(N - K) :
+    total.append(total[i] - ondo[i] + ondo[i + K])
+print(max(total))

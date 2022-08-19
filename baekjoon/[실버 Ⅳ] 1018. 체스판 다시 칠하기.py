96ca0cd1faeb4c88f79543bc538289d31fd54927
nayeonkinn <nayeonkinn@gmail.com>
2022-08-19 17:47:47 +0900

    220819

--- baekjoon/[실버 Ⅳ] 1018. 체스판 다시 칠하기.py
+++ baekjoon/[실버 Ⅳ] 1018. 체스판 다시 칠하기.py
@@ -5,8 +5,12 @@ def check(chess):
     cnt = 0
     for i in range(8):
         for j in range(8):
-            if chess[i][j] != 'WB'[(i + j) % 2]:
+            if (i + j) % 2 == 0 and chess[i][j] != 'W':
                 cnt += 1
+            elif (i + j) % 2 == 1 and chess[i][j] != 'B':
+                cnt += 1
+            # if chess[i][j] != 'WB'[(i + j) % 2]: # 한번에 표현도 가능
+            #     cnt += 1
     return cnt if cnt <= 32 else 64 - cnt
 
 N, M = map(int, input().split())

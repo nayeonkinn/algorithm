75522359ba143605738f2dd8d63d55127d4b4b30
김나연 <nayeonkinn@gmail.com>
2022-09-28 21:44:39 +0900

    220928

--- swea/[D4] 5247. 연산.py
+++ swea/[D4] 5247. 연산.py
@@ -12,11 +12,9 @@ def bfs():
             return cnt
         
         for w in [v + 1, v - 1, v * 2, v - 10]:
-            if 0 < w <= M + 10 and visited[w] == 0:
+            if 0 < w <= min(M + 10, 1000000) and visited[w] == 0:
                 queue.append((w, cnt + 1))
                 visited[w] = 1
-                if w == M:
-                    return cnt + 1
 
 T = int(input())
 for tc in range(1, T + 1):

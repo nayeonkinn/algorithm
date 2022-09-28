da71000453ef93db7fa1ae9b82e5d58f9af6bb53
김나연 <nayeonkinn@gmail.com>
2022-09-28 21:41:57 +0900

    220928

---
+++ swea/[D4] 5247. 연산.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('input/5247.txt')
+
+def bfs():
+    queue = [(N, 0)]
+    visited = [0] * 1000001
+    visited[N] = 1
+
+    while queue:
+        v, cnt = queue.pop(0)
+        if v == M:
+            return cnt
+        
+        for w in [v + 1, v - 1, v * 2, v - 10]:
+            if 0 < w <= M + 10 and visited[w] == 0:
+                queue.append((w, cnt + 1))
+                visited[w] = 1
+                if w == M:
+                    return cnt + 1
+
+T = int(input())
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    print(f'#{tc} {bfs()}')

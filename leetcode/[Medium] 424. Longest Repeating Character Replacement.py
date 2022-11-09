92f639e0f0fab21abd9c1d6b06c0f847b23f5f7e
김나연 <nayeonkinn@gmail.com>
2022-11-09 23:58:27 +0900

    221109

---
+++ leetcode/[Medium] 424. Longest Repeating Character Replacement.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        counter = collections.Counter()
+        
+        for right in range(1, len(s) + 1):
+            counter[s[right - 1]] += 1
+            max_char_n = counter.most_common(1)[0][1]
+            
+            if right - left - max_char_n > k:
+                counter[s[left]] -= 1
+                left += 1
+        
+        return right - left

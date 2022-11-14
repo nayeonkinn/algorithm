2f0e6f2291a772b9298686590a09c6ff64d3fcf0
김나연 <nayeonkinn@gmail.com>
2022-11-14 17:56:41 +0900

    221114

--- baekjoon/[골드 Ⅰ] 2536. 버스 갈아타기.py
+++ baekjoon/[골드 Ⅰ] 2536. 버스 갈아타기.py
@@ -1,4 +1,14 @@
 import sys
 sys.stdin = open('input/2536.txt')
 
-print(input())
+m, n = map(int, input().split())
+k = int(input())
+bus_info, adj = {}, {}
+for _ in range(k):
+    b, x1, y1, x2, y2 = map(int, input().split())
+    bus_info[b] = ((x1, y1, x2, y2))
+    adj[b] = []
+sx, sy, dx, dy = map(int, input().split())
+
+for bus in bus_info:
+    print(bus)

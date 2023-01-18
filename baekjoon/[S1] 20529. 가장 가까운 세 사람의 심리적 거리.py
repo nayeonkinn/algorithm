5877fcc1aca3632b2adbff3fb686b109ed264361
김나연 <nayeonkinn@gmail.com>
2023-01-18 21:43:16 +0900

    230118

--- baekjoon/[S1] 20529. 가장 가까운 세 사람의 심리적 거리.py
+++ baekjoon/[S1] 20529. 가장 가까운 세 사람의 심리적 거리.py
@@ -1,20 +1,20 @@
 import sys
+from itertools import combinations as comb
 sys.stdin = open('input/20529.txt')
 input = sys.stdin.readline
+
+def d(A, B):
+    return sum([A[i] != B[i] for i in range(4)])
 
 T = int(input())
 for tc in range(T):
     N = int(input())
     mbti = input().split()
-    print(mbti)
-
-    for i in range(N << 2):
-        cnt = 0
-        for j in range(N):
-            if i & (1 << j):    
-                cnt += 1
-        if cnt == 3:
-            for k in range(N):
-                if len(bin(i)) > 2 + k and bin(i)[2 + k] == '1':
-                    print(3)
-            print(bin(i))
+    if len(mbti) > 32:
+        print(0)
+    else:
+        ans = 12
+        for c in set(list(comb(mbti, 3))):
+            dist = d(c[0], c[1]) + d(c[0], c[2]) + d(c[1], c[2])
+            ans = min(ans, dist)
+        print(ans)

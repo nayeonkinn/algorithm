d0dce7b17400ee980bf1a7e232267b4953734633
nayeonkinn <nayeonkinn@gmail.com>
2023-06-15 23:57:36 +0900

    230615

--- baekjoon/[G5] 12865. 평범한 배낭-2.py
+++ baekjoon/[G5] 12865. 평범한 배낭-2.py
@@ -6,11 +6,8 @@ table = [0] * (k + 1)
 
 for _ in range(n):
     w, v = map(int, input().split())
-    if w > k:
-        continue
-    for j in range(k, 0, -1):
-        if j + w <= k and table[j] != 0:
+    for j in range(k, -1, -1):
+        if j + w <= k:
             table[j + w] = max(table[j + w], table[j] + v)
-    table[w] = max(table[w], v)
 
 print(max(table))

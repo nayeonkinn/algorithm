779c9e4763095177d231773a11b84486e31a0d9e
nayeonkinn <nayeonkinn@gmail.com>
2023-12-29 14:09:52 +0900

    231229

---
+++ baekjoon/[S1] 1932. 정수 삼각형.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('input/1932.txt')  # 30
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = []
+for i in range(n):
+    dp.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(i + 1):
+        if j == 0:
+            dp[i][j] += dp[i - 1][j]
+        elif j == i:
+            dp[i][j] += dp[i - 1][j - 1]
+        else:
+            dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j])
+
+print(max(dp[-1]))

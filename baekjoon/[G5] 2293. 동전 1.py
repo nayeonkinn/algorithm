d01ea7866df8f15562e82a50fc8a58e75c1849a0
nayeonkinn <nayeonkinn@gmail.com>
2024-05-16 16:25:09 +0900

    240516

---
+++ baekjoon/[G5] 2293. 동전 1.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stdin = open('input/2293.txt')  # 10
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+dp = [1] + [0] * k
+
+for coin in coins:
+    for i in range(coin, k + 1):
+        dp[i] += dp[i - coin]
+
+print(dp[-1])
